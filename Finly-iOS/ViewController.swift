3a9c219fd57ac360b6d5a2e7b946ca0c634e1536
m1keee3 <skirdamisa@gmail.com>
2026-02-25 12:48:52 +0300

    Initial Commit

---
+++ Finly-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finly-iOS
+//
+//  Created by Michael Skirda on 25.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
